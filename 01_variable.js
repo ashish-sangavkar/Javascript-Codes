95d5be2324c327b9371a7ec0062db367b7204097
ashish-sangavkar <ashish.sangavkar47@gmail.com>
2024-02-02 23:59:22 +0530

    variables

---
+++ 01_variable.js
@@ -0,0 +1,20 @@
+const accountNumber = 145342
+let accountEmail = "ashish@gmail.com11"
+var accountPassword = "32112311"
+accountCity = "Miraj"
+
+let accountState // this will print undefined because value is not defined just declared variable
+
+
+//accountNumber = 12345 // we cannot change value of account number because it is constant
+
+accountEmail = "Ashs@gmail.com"
+accountPassword = "asjiej455"
+accountCity = "Sangli"
+
+console.log(accountNumber)
+
+console.table([accountEmail, accountNumber, accountPassword, accountCity, accountState])
+
+//prefere not to use var because it cannot create scope problem
+
