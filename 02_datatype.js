b8fc3fc2a1f1b86288c81c94fb4e71ec0bfaa406
ashish-sangavkar <ashish.sangavkar47@gmail.com>
2024-02-03 14:53:24 +0530

    02_dataTypex

---
+++ 02_datatype.js
@@ -0,0 +1,15 @@
+"use strict" // use all js code as newer version
+
+//alert("hii") // we are at node not on browser
+
+//Data types:
+    //number 2 pow 53
+    // bigint
+    // string = ""
+    //boolean = true/false
+    //null = standalone value
+    // undefined = 
+    // symbol = used for unique
+
+console.log(typeof undefined) // prints undefined type of undefined is undefined
+console.log(typeof null);
