ebddc8b0d52c38abf75d1d8e8bb6d6532a10a00c
ashish-sangavkar <ashish.sangavkar47@gmail.com>
2024-02-03 15:13:59 +0530

    02.1_datatypes

--- 02_datatype.js
+++ 02_datatype.js
@@ -1,4 +1,4 @@
-"use strict" // use all js code as newer version
+"use strict"; // use all js code as newer version
 
 //alert("hii") // we are at node not on browser
 
@@ -11,5 +11,16 @@
     // undefined = 
     // symbol = used for unique
 
-console.log(typeof undefined) // prints undefined type of undefined is undefined
-console.log(typeof null);
+
+    let a = 1234;
+    let b = "ashish";
+    let c = true;
+    let d ;
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+console.log(typeof a); // number
+console.log(typeof b); // string
+console.log(typeof c); // boolean
+console.log(typeof d); //undefined
