fe60c8ecf6bc91a7d675fb8623b42047409910fc
ashish-sangavkar <ashish.sangavkar47@gmail.com>
2024-02-03 16:59:36 +0530

    Data types

---
+++ 04_datatypesdetail.js
@@ -0,0 +1,13 @@
+
+// categories using how data is stored in memry and how that accessble
+
+// Primitive
+
+    //String , Nymber, Boolean, null, undefined, symbol, BigInt
+
+// Reference/ Non Primitive
+
+    //Array, Ojects, Functions
+
+
+    
