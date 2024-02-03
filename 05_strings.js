dfded1171a3c3a78068ba22d5c189b011e519ca7
ashish-sangavkar <ashish.sangavkar47@gmail.com>
2024-02-04 00:59:47 +0530

    String operations

---
+++ 05_strings.js
@@ -0,0 +1,48 @@
+const name = "Ashish";
+
+const birthYear = 2001;
+
+console.log(name + " "+ birthYear); // 1st way but not recomended
+
+console.log(`My name is ${name} and my birth year is ${birthYear}`);  // 2nd way to print and preffered way backticks(``) is used for this
+
+console.log();
+
+// another way to declare String
+
+let newName = new String("Ashish Sangavkar");
+
+console.log(newName);  // [String: 'Ashish Sangavkar']
+
+console.log(newName.charAt(3)); //i
+
+console.log(newName.toLowerCase()); //ashish sangavkar
+
+console.log(newName.toUpperCase()); //ASHISH SANGAVKAR
+
+console.log(newName.substring(1,6)); //shish
+
+console.log(newName.replace("sh", "oo")); //Aooish Sangavkar
+
+console.log(newName.concat(" wk batsman")); // Ashish Sangavkar wk batsman
+
+console.log(newName.endsWith(("r"))); //true
+
+console.log(newName.indexOf("s")); // 1
+ 
+let stud = "    sjshjs   ";
+
+console.log(stud); //   |    sjshjs     |
+console.log(stud.trim()); // |sjshjs| removes spaces before and after string
+
+console.log(newName.includes("ish")); // true
+
+let gmail = "ashish-san-gavkar-47@gmail.com";
+
+console.log(gmail.split("-"));  //  [ 'ashish', 'san', 'gavkar', '47@gmail.com' ]
+
+console.log(gmail.search("r")); //  16
+
+
+
+
