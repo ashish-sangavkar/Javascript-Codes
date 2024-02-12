9760e1b9f45b405a3830755c3f9535baf6e30052
ashish-sangavkar <ashish.sangavkar47@gmail.com>
2024-02-12 11:08:44 +0530

    objects - 12-02-24 11:08

---
+++ 10_objects_01.js
@@ -0,0 +1,36 @@
+
+let mySym = Symbol('sym');
+
+let me = {
+    "name" : "Ashish",
+    age : 23,
+    [mySym] : "MSD",
+    "mobile" : 7038201515,
+    city : "sangli",
+    language : ["marathi", "hindi"],
+    "college" : "ADCET"
+}
+
+console.log(me.name); //
+console.log(me['college']); //
+
+me.age = 24
+
+//to change the value 
+console.log(me);
+
+//Object.freeze(me);
+
+me.age = 30;
+console.log(me);
+
+me.wish = function(){
+    console.log("HAppy birthday");
+}
+console.log(me.wish);
+
+me.wish2 = function(){
+    console.log("welcome "+this.name);
+}
+
+console.log(me.wish2());
